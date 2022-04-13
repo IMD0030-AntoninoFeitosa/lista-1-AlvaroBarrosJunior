30fa25b94e657dfa4620fed1d11d91b1065545b1
AlvaroBarrosJunior <alvarojoaquimjr@gmail.com>
2022-04-12 21:51:33 -0300

    mais algumas questoes

--- ponto_em_retangulo_1/src/function.cpp
+++ ponto_em_retangulo_1/src/function.cpp
@@ -1,13 +1,21 @@
 #include "function.h"
 
-/*! 
+/*!
  * Verifica se um ponto está dentro de um retângulo.
  */
 
-location_t pt_in_rect( const Ponto &IE, const Ponto &SD, const Ponto &P )
+namespace function
 {
-    // TODO: Coloque aqui seu código.
-
-    // TODO: Substitua o retorno conforme desejar. Isso é apenas um STUB, usado apenas para compilar.
-    return location_t::OUTSIDE;
+    location_t pt_in_rect(const Ponto &IE, const Ponto &SD, const Ponto &P)
+    {
+        if (P.x > IE.x && P.y > IE.y && P.x < SD.x && P.y < SD.y){
+            return location_t::INSIDE;
+        }
+        else if (P.x >= IE.x && P.y >= IE.y && P.x <= SD.x && P.y <= SD.y){
+            return location_t::BORDER;
+        }
+        else {
+            return location_t::OUTSIDE;
+        }
+    }
 }
--- ponto_em_retangulo_2/src/main.cpp
+++ ponto_em_retangulo_2/src/main.cpp
@@ -4,24 +4,64 @@
  * @data June, 6th 2021
  */
 
+#include <vector>
 #include <iostream>
-using std::cout;
-using std::cin;
-using std::endl;
-#include <algorithm>
-using std::min;
-using std::max;
-
 #include "function.h"
+using namespace std;
+
 
 //=== Funções
 
 // Coloque aqui qualquer função auxiliar que desejar.
 
-
 int main(void)
 {
-    // TODO: Adicione aqui seu código.
+    function::Ponto IE, SD, p;
+    function::location_t local;
+    vector<int> vetor;
+
+    int x;
+    while (cin >> std::ws >> x)
+    {
+        vetor.push_back(x);
+    }
+
+    if (vetor.size() % 6 != 0)
+    {
+        cout << "Entrada incorreta!" << endl;
+    }
+    else
+    {
+        for (int i = 0; i < vetor.size(); i = i + 6)
+        {
+            IE.x = vetor[i];
+            IE.y = vetor[i + 1];
+            SD.x = vetor[i + 2];
+            SD.y = vetor[i + 3];
+            p.x = vetor[i + 4];
+            p.y = vetor[i + 5];
+
+            if (IE.x > SD.x)
+            {
+                int temp;
+                temp = IE.x;
+                IE.x = SD.x;
+                SD.x = temp;
+            }
+            if (IE.y > SD.y)
+            {
+                int temp;
+                temp = IE.y;
+                IE.y = SD.y;
+                SD.y = temp;
+            }
+
+            local = function::pt_in_rect(IE, SD, p); 
+            cout << local << endl;
+        }
+    }
+
+    
 
     return 0;
 }
